68a5e4cb35fe77ff2e5840d4aa432bafc4315388
bank25400 <kantapong40202@gmail.com>
2024-12-10 11:27:45 +0700

    front end done

--- frontend/src/components/cardList.tsx
+++ frontend/src/components/cardList.tsx
@@ -1,27 +1,30 @@
 import { Card, CardMedia, CardContent, Typography, Grid } from "@mui/material";
+import { ImageItemType } from "../pages/home";
 
-const CardList = (props: any) => {
-    return (
-        <Grid container spacing={1} data-testid="images-list">
-            <Grid item xs={12} md={4} data-testid="image-item">
-                <Card variant="outlined">
-                    <CardMedia
-                        component="img"
-                        alt="image-alt"
-                        height={300}
-                        width="100%"
-                        image="url"
-                        loading="lazy"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h6" component="div">
-                            Size: width x height
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Grid>
+const CardList = ({ images }: { images: ImageItemType[] }) => {
+  return (
+    <Grid container spacing={1} data-testid="images-list">
+      {images?.map((imageItem: ImageItemType) => (
+        <Grid key={imageItem.id} item xs={12} md={4} data-testid="image-item">
+          <Card variant="outlined">
+            <CardMedia
+              component="img"
+              alt="image-alt"
+              height={300}
+              width="100%"
+              image={imageItem.url}
+              loading="lazy"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h6" component="div">
+                Size: {imageItem.width.toLocaleString()} x {imageItem.height.toLocaleString()}
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
-    );
+      ))}
+    </Grid>
+  );
 };
 
 export default CardList;
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,44 +1,93 @@
 import {
-    Box,
-    Button,
-    LinearProgress,
-    Stack,
-    TextField,
-    Typography,
+  Box,
+  Button,
+  LinearProgress,
+  Stack,
+  TextField,
+  Typography,
 } from "@mui/material";
 import CardList from "../components/cardList";
+import { useState } from "react";
+
+export interface ImageItemType {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
+
+const API_KEY =
+  "live_UMIEHUqwTGnUFY8TiJrv60nSuK36A42ddgNtFcb9YWb4lsQgXUJGvfJ4xcJfnlqA";
 
 const Home = () => {
-    return (
-        <Stack spacing={2} padding={1} data-testid="main">
-            <Typography variant="h3" component="h1">
-                Cat Gallery
-            </Typography>
-            <Stack>
-                <TextField
-                    fullWidth
-                    inputProps={{
-                        "data-testid": "images-number-field",
-                    }}
-                    label="Images Number"
-                    type="number"
-                    helperText={"Number should be between 1 and 10"}
-                />
-                <Button
-                    data-testid="random-image-btn"
-                    disableElevation
-                    variant="contained"
-                    fullWidth
-                >
-                    Random
-                </Button>
-            </Stack>
-            <Box sx={{ width: "100%" }} data-testid="loading-indicator">
-                <LinearProgress />
-            </Box>
-            <CardList />
-        </Stack>
-    );
+  const [images, setImages] = useState<ImageItemType[]>([]);
+  const [numberValue, setNumberValue] = useState<string>("1");
+  const [isErrorNumber, setIsErrorNumber] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const getRandomImage = async () => {
+    try {
+      const response = await fetch(
+        `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&limit=${numberValue}`
+      );
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setImages(data);
+    } catch (error) {
+      console.error("error", error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const onClickRandom = () => {
+    const parseNumber = Number(numberValue);
+    setIsLoading(true);
+    if (parseNumber < 1 || parseNumber > 10) {
+      setIsErrorNumber(true);
+      setIsLoading(false);
+    } else {
+      setIsErrorNumber(false);
+      getRandomImage();
+    }
+  };
+
+  return (
+    <Stack spacing={2} padding={1} data-testid="main">
+      <Typography variant="h3" component="h1">
+        Cat Gallery
+      </Typography>
+      <Stack>
+        <TextField
+          error={isErrorNumber}
+          fullWidth
+          inputProps={{
+            "data-testid": "images-number-field",
+          }}
+          label="Images Number"
+          type="number"
+          helperText={isErrorNumber && "Number should be between 1 and 10"}
+          value={numberValue}
+          onChange={(e) => setNumberValue(e.target.value)}
+        />
+        <Button
+          data-testid="random-image-btn"
+          disableElevation
+          variant="contained"
+          fullWidth
+          onClick={onClickRandom}
+        >
+          Random
+        </Button>
+      </Stack>
+      <Box sx={{ width: "100%" }} data-testid="loading-indicator">
+        {isLoading && <LinearProgress />}
+      </Box>
+      <CardList images={images} />
+    </Stack>
+  );
 };
 
 export default Home;
